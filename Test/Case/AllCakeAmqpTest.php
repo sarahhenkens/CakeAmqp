818231b458ebe1752aceef649c108ba8b8f5273b
Jelle Henkens <jelle.henkens@gmail.com>
2012-05-27 12:38:30 +0100

    Adding travis-ci configuration

---
+++ Test/Case/AllCakeAmqpTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class AllCakeAmqpTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All CakeAmqp tests');
+
+		$path = CakePlugin::path('CakeAmqp') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $suite;
+	}
+}
